eb8eda7f3458a2ad7f001214b0ee3f396bba075e
kdnakt <a.kid.1985@gmail.com>
2020-07-24 00:40:51 +0900

    rss: not enough items, returns this Monday

--- rss.test.ts
+++ rss.test.ts
@@ -6,3 +6,13 @@ test('no items field Parser.Output returns this monday', () => {
   const monday = getThisMonday(moment());
   expect(getLatestFeedPubDate({}, 1).isSame(monday)).toBe(true);
 });
+
+test('not enough items for requiredCount + 1, returns this monday', () => {
+  const monday = getThisMonday(moment());
+  const output = {
+      items: [{
+          pubDate: '2020-07-23T15:00:01.000Z'
+      }]
+  };
+  expect(getLatestFeedPubDate(output, 1).isSame(monday)).toBe(true);
+});
--- rss.ts
+++ rss.ts
@@ -6,8 +6,8 @@ import { getThisMonday } from './date';
  * 最新フィードの公開日を取得する
  */
 export const getLatestFeedPubDate = (output: Parser.Output, requiredCount: number) => {
-  console.log(requiredCount);
-  if (!output.items) {
+  if (!output.items|| output.items.length < requiredCount + 1) {
+    // そもそも記事数が足りない場合は公開日を取得できないのでlatestは、必ず通知対象となる今週の月曜日と合わせる
     return getThisMonday(moment());
   }
   return moment();
