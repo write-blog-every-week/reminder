5483b9e0336e8d38c6c607936053c1c929ab50c7
kdnakt <a.kid.1985@gmail.com>
2020-07-24 00:56:19 +0900

    remove moment() as default argument

--- rss.test.ts
+++ rss.test.ts
@@ -3,12 +3,12 @@ import * as moment from 'moment';
 import { getLatestFeedPubDate } from "./rss";
 
 test('no items field Parser.Output returns this monday', () => {
-  const monday = getThisMonday(moment());
+  const monday = getThisMonday();
   expect(getLatestFeedPubDate({}, 1).isSame(monday)).toBe(true);
 });
 
 test('not enough items for requiredCount + 1, returns this monday', () => {
-  const monday = getThisMonday(moment());
+  const monday = getThisMonday();
   const output = {
       items: [{
           pubDate: '2020-07-23T15:00:01.000Z'
--- rss.ts
+++ rss.ts
@@ -8,7 +8,7 @@ import { getThisMonday } from './date';
 export const getLatestFeedPubDate = (output: Parser.Output, requiredCount: number) => {
   if (!output.items|| output.items.length < requiredCount + 1) {
     // そもそも記事数が足りない場合は公開日を取得できないのでlatestは、必ず通知対象となる今週の月曜日と合わせる
-    return getThisMonday(moment());
+    return getThisMonday();
   }
   return moment(output.items[requiredCount].pubDate);
 }
