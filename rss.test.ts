83314391c10e09eb499766a539f43d48eb7a7790
kdnakt <a.kid.1985@gmail.com>
2020-07-24 00:50:22 +0900

    implement getLatestFeedPubDate

--- rss.test.ts
+++ rss.test.ts
@@ -16,3 +16,17 @@ test('not enough items for requiredCount + 1, returns this monday', () => {
   };
   expect(getLatestFeedPubDate(output, 1).isSame(monday)).toBe(true);
 });
+
+test('2 items required, returns pubDate of 3rd item', () => {
+  const expected = moment('2020-07-21T15:00:01.000Z');
+  const output = {
+      items: [{
+          pubDate: '2020-07-23T15:00:01.000Z'
+      }, {
+          pubDate: '2020-07-22T15:00:01.000Z'
+      }, {
+          pubDate: '2020-07-21T15:00:01.000Z'
+      }]
+  };
+  expect(getLatestFeedPubDate(output, 2).isSame(expected)).toBe(true);
+});
