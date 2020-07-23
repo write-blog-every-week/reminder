cb020463293730eba0d54a925622f5c533656f47
kdnakt <a.kid.1985@gmail.com>
2020-07-23 11:04:42 +0900

    add test for getLastWeekMonday

--- date.ts
+++ date.ts
@@ -1,5 +1,9 @@
 import * as moment from 'moment';
 
+/**
+ * 今週の月曜の日付を取得する
+ * @param nowDate
+ */
 export const getThisMonday = (nowDate: moment.Moment) => {
   let day = nowDate.get('day') - 1;
   if (day === -1) day = 6;
@@ -8,5 +12,13 @@ export const getThisMonday = (nowDate: moment.Moment) => {
   nowDate.set('minute', 0);
   nowDate.set('second', 0);
   nowDate.set('millisecond', 0);
+  return nowDate;
+}
+
+/**
+ * 1週間前の月曜日を取得する(ロジック的には月曜日固定ではないけど、lambdaが月曜日に実行されるからよしとする)
+ * @param nowDate
+ */
+export const getLastWeekMonday = (nowDate: moment.Moment) => {
   return nowDate;
 }
